21996df3bd401f3ee0c628ab893fcd79cc68dcb1
Raquel Dunoff <raquel@students.olin.edu>
2019-03-07 22:30:51 -0500

    AVL Trees

--- trees/day01/src/AVLTree.java
+++ trees/day01/src/AVLTree.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.max;
+
 public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
     /**
@@ -10,6 +12,9 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
             // TODO
             // update the height of the tree using the height of the left and right child
             // return balance(n)
+
+            n.height =  max(height(n.leftChild),height(n.rightChild)) + 1;
+            return balance(n);
         }
         return null;
     }
@@ -24,8 +29,12 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
             // TODO
             // update the height of the tree using the height of the left and right child
             // return balance(n)
+
+            n.height =  max(height(n.leftChild),height(n.rightChild)) + 1;
+            return balance(n);
         }
         return null;
+
     }
 
     /**
@@ -35,7 +44,7 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
     TreeNode<T> deleteMin(TreeNode<T> n) {
         n = super.deleteMin(n);
         if (n != null) {
-            n.height = 1 + Math.max(height(n.leftChild), height(n.rightChild));
+            n.height = 1 + max(height(n.leftChild), height(n.rightChild));
             return balance(n);
         }
         return null;
@@ -43,18 +52,31 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
     // Return the height of the given node. Return -1 if null.
     private int height(TreeNode<T> n) {
-        // TODO
-        return 0;
+        if(n==null) return -1; //if it is empty, return -1
+        return n.height;
+        //return max(height(n.leftChild),height(n.rightChild)) + 1; //if there is both, return the taller child + 1
     }
 
     public int height() {
-        return Math.max(height(root), 0);
+        return max(height(root), 0);
     }
 
     // Restores the AVL tree property of the subtree. Return the head of the new subtree
     TreeNode<T> balance(TreeNode<T> n) {
         // TODO: (if you're having trouble, use pseudocode provided in slides)
-        return null;
+        if(balanceFactor(n) > 1){ //If it is right heavy, rotate left
+            if(balanceFactor(n.rightChild) <= -1){
+                n.rightChild = rotateRight(n.rightChild); //second rotation for correction
+            }
+            n = rotateLeft(n);
+        }
+        if(balanceFactor(n)<-1){ //if it is left heavy, rotate right
+            if(balanceFactor(n.leftChild) >= 1){
+                n.leftChild = rotateLeft(n.leftChild); //second rotation for correction
+            }
+            n = rotateRight(n);
+        }
+        return n;
     }
 
     /**
@@ -65,23 +87,41 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
      * most one.
      */
     private int balanceFactor(TreeNode<T> n) {
-        // TODO
-        return 0;
+        if(n==null) return -1;
+        int rightHeight = -1;
+        int leftHeight = -1;
+        if(n.hasRightChild()){
+            rightHeight = n.rightChild.height;
+        }
+        if(n.hasLeftChild()){
+            leftHeight = n.leftChild.height;
+        }
+        return rightHeight - leftHeight;
     }
 
     /**
      * Perform a right rotation on node `n`. Return the head of the rotated tree.
      */
     private TreeNode<T> rotateRight(TreeNode<T> n) {
-        // TODO
-        return null;
+        TreeNode<T> y = n.leftChild;
+        TreeNode<T> beta = y.rightChild;
+        y.rightChild = n;
+        n.leftChild = beta;
+        n.height = max(height(n.leftChild),height(n.rightChild)) + 1;
+        y.height = max(height(y.leftChild),height(y.rightChild)) + 1;
+        return y;
     }
 
     /**
      * Perform a left rotation on node `n`. Return the head of the rotated tree.
      */
     private TreeNode<T> rotateLeft(TreeNode<T> n) {
-        // TODO
-        return null;
+        TreeNode<T> y = n.rightChild;
+        TreeNode<T> beta = y.leftChild;
+        y.leftChild = n;
+        n.rightChild = beta;
+        n.height = max(height(n.leftChild),height(n.rightChild)) + 1;
+        y.height = max(height(y.leftChild),height(y.rightChild)) + 1;
+        return y;
     }
 }
--- trees/day01/src/Problems.java
+++ trees/day01/src/Problems.java
@@ -1,7 +1,25 @@
+import static java.util.Arrays.sort;
+
 public class Problems {
 
     public static int leastSum(int[] A) {
         //TODO
-        return 0;
+        sort(A);
+
+        int a = 0;
+        int b = 0;
+        int place = 1;
+        if(A.length == 0) return 0;
+        if(A.length == 1) return A[0];
+
+        for(int i=A.length-1; i>0; i-=2){
+            a += place * A[i];
+            b += place * A[i-1];
+            place  = place * 10;
+        }
+
+        if(A.length % 2 == 1) a += place * A[0];
+
+        return a+b;
     }
 }
