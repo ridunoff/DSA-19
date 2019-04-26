1b526473c3a15f5e53e7fee8d97fea00dacf6d2b
Raquel Dunoff <raquel@students.olin.edu>
2019-04-25 22:41:31 -0400

    DP

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,43 @@
+import java.util.*;
+
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(n)
+    // Space: O(n)
+    //static int total=0;
+    //static List<Integer> rolls = List.of(1,2,3,4,5,6);
+    static HashMap<Integer,Integer> dpMap = new HashMap<Integer, Integer>();
+
+
     public static int diceRollSum(int N) {
         // TODO
-        return 0;
-    }
+        //List<Integer> rolls = List.of(1,2,3,4,5,6);
+
+        return diceRollHelper(N, 0);
+
+        }
+
+        public static int diceRollHelper(int N, int sum){
+
+            if(dpMap.containsKey(N)){
+                return dpMap.get(N);
+            }
+            else if(N==0){
+                dpMap.put(N,1);
+                return 1;
+            }
+            else if(N<0){
+                return 0;
+            }
+            else{
+                for(int j=1;j<=6;j++) {
+                    sum += diceRollSum(N - j);
+
+                }
+                dpMap.put(N,sum);
+            }
+            return dpMap.get(N);
+
+        }
 
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,41 @@
+import java.util.HashMap;
+
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(n^2)
+    // Space: O(n)
+
+
     public static int LIS(int[] A) {
         // TODO
-        return 0;
+
+        if(A.length==0) return 0;
+
+        int[] count = new int[A.length];
+
+        for(int i=1; i<A.length; i++){
+            for(int j=0; j<i; j++){
+                //System.out.println(A[j]);
+                if(A[j]<A[i] && count[i]<count[j]+1){
+                    count[i]++;
+
+                }
+            }
+        }
+
+
+        int max = count[0];
+
+        for(int i=1; i<A.length; i++){
+            System.out.println(count[i]);
+            if(count[i]>max){
+                max = count[i];
+            }
+        }
+
+        return max+1;
+
     }
+
+
 }
